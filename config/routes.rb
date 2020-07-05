c8f34edd883c36e31b1636bd6682ede918e647ed
vagrant <vagrant@localhost.localdomain>
2020-07-05 12:58:31 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   get 'home/about'
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+  get 'users/:id/follow' => 'users#follow', as: 'follows'
+  get 'users/:id/follower' => 'users#follower', as: 'followers'
   resources :users, only: [:show, :index, :edit, :update]
   resources :books, except: [:new] do
     resources :book_comments, only: [:create, :destroy]
